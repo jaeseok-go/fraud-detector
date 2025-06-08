6b67b161e3114c7d43b62e6ee8762a2a3b2146df
jaeseok-go <gojeasuk2@naver.com>
2025-06-08 21:54:09 +0900

    chore: profile-detect-service 모듈 초기 설정

---
+++ profile-detect-service/src/test/java/com/demo/profiledetectservice/ProfileDetectServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.profiledetectservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProfileDetectServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
