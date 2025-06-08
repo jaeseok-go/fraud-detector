0348b53e21bd11f808e232b108ef433b89c9c3cf
jaeseok-go <gojeasuk2@naver.com>
2025-06-08 21:50:00 +0900

    chore: rule-service 모듈 초기 설정

---
+++ rule-service/src/test/java/com/demo/ruleservice/RuleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.ruleservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuleServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
